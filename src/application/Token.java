a3e4ec630d64ab70c1c7d190dff3a37f4a620049
xieyongshen <786102508@qq.com>
2018-10-28 16:07:37 +0800

    Lexer with 3 more precise types: keywords, operator and EOL.

---
+++ src/application/Token.java
@@ -0,0 +1,44 @@
+package application;
+
+public abstract class Token {
+	
+	public static final Token EOF = new Token(-1) {};
+	public static final String EOL = "\\n";
+	private int lineNumber;
+
+	public Token(int line) {
+		lineNumber = line;
+	}
+	
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	public int getType() { return 0; }
+
+	public boolean isIdentifier() {
+		return false;
+	}
+	
+	public boolean isNumber() {
+		return false;
+	}
+	
+	public boolean isString() {
+		return false;
+	}
+
+	public boolean isKeyword() { return false; }
+
+	public boolean isOperator() { return false; }
+
+	public boolean isEOL() { return false; }
+	
+	public int getNumber() {
+		throw new RuntimeException("not number token");
+	}
+	
+	public String getText() {
+		return "";
+	}
+}
