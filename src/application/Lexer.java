d4b1e17fe5d863baf365782270369e7a79aeae42
VorSonnenaufgang <875503176@qq.com>
2018-10-29 11:57:41 +0800

    Fix Chinise again

--- src/application/Lexer.java
+++ src/application/Lexer.java
@@ -10,80 +10,85 @@ import java.util.regex.Pattern;
 public class Lexer {
 	public static String regexPat = "\\s*((//.*)|([0-9]+)|(\"(\\\\\"|\\\\\\\\|\\\\n|[^\"])*\")"
 			+ "|[A-Z_a-z][A-Z_a-z0-9]*|\\+=|-=|\\*=|/=|%=|\\+\\+|--|==|<=|>=|&&|\\|\\||\\p{Punct})?";
-	public static String regexChina = "^[\\u4e00-\\u9fa5]";
-	
+	public static String regexChina = "[\\u4e00-\\u9fa5\\u3002\\uff1b\\uff0c\\uff1a\\u201c\\u201d\\uff08\\uff09\\u3001\\uff1f\\u300a\\u300b]";
+	public static String regexChinaStr = "\"[\\u4e00-\\u9fa5\\u3002\\uff1b\\uff0c\\uff1a\\u201c\\u201d\\uff08\\uff09\\u3001\\uff1f\\u300a\\u300b]*\"";
+
 	private Pattern pattern = Pattern.compile(regexPat);
 	private Pattern patternChina = Pattern.compile(regexChina);
+	private Pattern patternChinaStr = Pattern.compile(regexChinaStr);
+
 	
 	private ArrayList<Token> queue = new ArrayList<Token>();
 	private boolean hasMore;
 	private LineNumberReader reader;
-	
+
 	public Lexer(Reader r) {
 		hasMore = true;
 		reader = new LineNumberReader(r);
 	}
-	
+
 	public Token read() throws Exception {
-		
-		if(fillQueue(0))
+
+		if (fillQueue(0))
 			return queue.remove(0);
 		else
 			return Token.EOF;
 	}
-	
+
 	public Token peek(int i) throws Exception {
-		if(fillQueue(i))
+		if (fillQueue(i))
 			return queue.get(i);
 		else
 			return Token.EOF;
 	}
-	
+
 	private boolean fillQueue(int i) throws Exception {
 
-		while(i >= queue.size()) {
-			if(hasMore)
+		while (i >= queue.size()) {
+			if (hasMore)
 				readLine();
 			else
 				return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	protected void readLine() throws Exception {
 		String line;
-		
+
 		try {
 			line = reader.readLine();
-		}catch(IOException e) {
+		} catch (IOException e) {
 			throw new Exception(e);
 		}
-		
-		if(line == null) {
+
+		if (line == null) {
 			hasMore = false;
 			return;
 		}
-		
+
 		int lineNo = reader.getLineNumber();
-		Matcher matcher = pattern.matcher(line);
-		matcher.useTransparentBounds(true).useAnchoringBounds(false);
-		int pos = 0;
-		int endPos = line.length();
 		
 		Matcher matcherChina = patternChina.matcher(line);
-
-		if(matcherChina.lookingAt()) {
-			System.err.println("bad token at line " + lineNo);
+		Matcher matcherChinaStr = patternChinaStr.matcher(line);
+				
+		if(matcherChina.find()&&!matcherChinaStr.find()) {
+			System.err.println("Wrong identifier at line " + lineNo+ ", posiotion " + matcherChina.start());
+			line = matcherChina.replaceAll("");
 		}
-		
+
 		else {
-			while(pos < endPos) {
-				matcher.region(pos, endPos);			
-				if(matcher.lookingAt()) {
+			Matcher matcher = pattern.matcher(line);
+			matcher.useTransparentBounds(true).useAnchoringBounds(false);
+			int pos = 0;
+			int endPos = line.length();
+			while (pos < endPos) {
+				matcher.region(pos, endPos);
+				if (matcher.lookingAt()) {
 					addToken(lineNo, matcher);
 					pos = matcher.end();
-				}else {
+				} else {
 					throw new Exception("bad token at line " + lineNo);
 				}
 			}
@@ -91,32 +96,32 @@ public class Lexer {
 		
 		queue.add(new IdToken(lineNo, Token.EOL));
 	}
-	
+
 	protected void addToken(int lineNo, Matcher matcher) {
 		String m = matcher.group(1);
-		if(m != null)
-			if(matcher.group(2) == null) {
+		if (m != null)
+			if (matcher.group(2) == null) {
 				Token token;
-				if(matcher.group(3) != null)
+				if (matcher.group(3) != null)
 					token = new NumToken(lineNo, Integer.parseInt(m));
-				else if(matcher.group(4) != null)
+				else if (matcher.group(4) != null)
 					token = new StrToken(lineNo, toStringLiteral(m));
 				else
 					token = new IdToken(lineNo, m);
 				queue.add(token);
 			}
 	}
-	
+
 	protected String toStringLiteral(String s) {
 		StringBuilder sb = new StringBuilder();
 		int len = s.length() - 1;
-		for(int i=1; i<len; i++) {
+		for (int i = 1; i < len; i++) {
 			char c = s.charAt(i);
-			if(c == '\\' && i + 1 < len) {
+			if (c == '\\' && i + 1 < len) {
 				int c2 = s.charAt(i + 1);
-				if(c2 == '"' || c2 == '\\')
+				if (c2 == '"' || c2 == '\\')
 					c = s.charAt(++i);
-				else if(c2 == 'n') {
+				else if (c2 == 'n') {
 					i++;
 					c = '\n';
 				}
